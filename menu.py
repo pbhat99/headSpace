797b4512686c727abff7967fcc820033c371be19
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-03-26 12:43:07 +0530

    started
    setting up menu.py

---
+++ menu.py
@@ -0,0 +1,17 @@
+#Prasannakumar T Bhat
+#This is the main menu.py file to load into nuke
+
+#Let me confirm loading of this file in nuke terminal
+nuke.tprint('Loading Custom Tools by Prasannakumar T Bhat')
+
+#importing important modules
+import os
+import nuke
+
+#my knobdefaults & snippets
+import pbKnobDefaults
+import pbSnippets
+
+
+#confirm full file loaded properly
+nuke.tprint('pb tools Loaded!')
