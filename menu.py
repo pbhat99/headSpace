8735b0edf71c165787c21dcf41e32c4b95660949
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-04-14 14:33:32 +0530

    W_scaleTree & W_smartAlign
    Node arrange tools with added shortcuts

--- menu.py
+++ menu.py
@@ -24,7 +24,7 @@ import pbKnobDefaults
 nuke.menu("Nodes").addCommand('Other/Comma','import comma ; comma.makeComma()',',',icon='Comma.png',index=6)
 
 # dot create and align tool
-nuke.menu('Nuke').addCommand('-{ pb }-/Align Dots', "import AlignDots ; AlignDots.AlignDots()", 'Shift+.', shortcutContext=2)
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/Align Dots', "import AlignDots ; AlignDots.AlignDots()", 'Shift+.', shortcutContext=2)
 
 #better backdrop & stickyNotes
 nuke.menu('Nuke').addCommand('-{ pb }-/GrayAutoBackdrop', 'import GrayAutoBackdrop ; GrayAutoBackdrop.GrayAutoBackdrop()', 'alt+b', shortcutContext=2)
@@ -32,12 +32,20 @@ nuke.menu('Nuke').addCommand('-{ pb }-/GrayAutoBackdrop', 'import GrayAutoBackdr
 # pbSnippets 
 nuke.menu('Nuke').addCommand('-{ pb }-/GUI Disable','import pbSnippets ; pbSnippets.disableGUI()','shift+d')
 nuke.menu('Nodes').addCommand('Other/StickyNote','import pbSnippets ; pbSnippets.pasteNote()','alt+n')
-nuke.menu("Nuke").addCommand("Edit/Node/Label", "import pbSnippets ; pbSnippets.nLabel()","n")
+nuke.menu("Nuke").addCommand("-{ pb }-/NodeGraph/Change Label", "import pbSnippets ; pbSnippets.nLabel()","n")
 
 #framehold to current frame as default
 nuke.menu('Nodes').addCommand( "Time/FrameHold", "nuke.createNode('FrameHold')['first_frame'].setValue( nuke.frame() )", icon='FrameHold.png')
 
 
+#W_smartAlign
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/Align Left', 'import W_smartAlign ; W_smartAlign.alignNodes("left")',"Alt+Shift+left", shortcutContext=2)
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/Align Right', 'import W_smartAlign ; W_smartAlign.alignNodes("right")',"Alt+Shift+right", shortcutContext=2)
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/Align Up',	'import W_smartAlign ; W_smartAlign.alignNodes("up")', "Alt+Shift+up", shortcutContext=2)
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/Align Down', 'import W_smartAlign ; W_smartAlign.alignNodes("down")', "Alt+Shift+down", shortcutContext=2)
+
+#W_scaleTree
+nuke.menu('Nuke').addCommand('-{ pb }-/NodeGraph/W_scaleTree', 'import W_scaleTree ; W_scaleTree.scaleTreeFloatingPanel()', 'alt+`')
 
 
 #confirm full file loaded properly
