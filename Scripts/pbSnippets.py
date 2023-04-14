1ffc376d6eace27cdd32a0cc830f578f61c414e5
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-04-14 14:04:54 +0530

    $gui
    adde both functions for GUI disable

--- Scripts/pbSnippets.py
+++ Scripts/pbSnippets.py
@@ -6,13 +6,14 @@ import nuke
 #  $GUI Toggle
 #------------------------------------------------------------------------------
 def disableGUI():
-	for dis in nuke.selectedNodes():
-		if dis['disable'].hasExpression():
-			dis['disable'].clearAnimated()
-			dis['disable'].setValue(0)
-		else:
-			dis['disable'].setExpression('[python {1-nuke.executing()}]')
-	return
+    for dis in nuke.selectedNodes():
+        if dis['disable'].hasExpression():
+            dis['disable'].clearAnimated()
+            dis['disable'].setValue(0)
+        else:
+            dis['disable'].setExpression('$gui') # for general use
+            #dis['disable'].setExpression('[python {1-nuke.executing()}]') # if u need local render support
+    return
 
 
 
@@ -21,15 +22,15 @@ def disableGUI():
 #paste clipboard to stickyNote
 #------------------------------------------------------------------------------
 def pasteNote():
-  try:
-     from PySide2 import QtWidgets
-  except:
-     from PySide import QtGui as QtWidgets
-  clipboard = str('<align=left>') + '\n' + QtWidgets.QApplication.clipboard().text()
-  clipboard = clipboard.encode('utf-8')
-     #copyClip = QtGui.QApplication.clipboard().setText("Yay")
-  sn = nuke.createNode("StickyNote")
-  sn.knob('label').setValue(clipboard)
+    try:
+        from PySide2 import QtWidgets
+    except:
+        from PySide import QtGui as QtWidgets
+        clipboard = str('<align=left>') + '\n' + QtWidgets.QApplication.clipboard().text()
+        clipboard = clipboard.encode('utf-8')
+        #copyClip = QtGui.QApplication.clipboard().setText("Yay")
+    sn = nuke.createNode("StickyNote")
+    sn.knob('label').setValue(clipboard)
 
 
 
