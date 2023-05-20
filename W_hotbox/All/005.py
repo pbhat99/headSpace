37fcd92c308db1e7df4cbf1bb0ffbfb330b8181e
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-05-20 18:12:14 +0530

    Set IP w_hotbox button

--- W_hotbox/All/005.py
+++ W_hotbox/All/005.py
@@ -6,6 +6,19 @@
 #
 #----------------------------------------------------------------------------------------------------------
 
-sn = nuke.selectedNode().name()
-print (sn)
-nuke.toNode('Viewer1').knob("input_process_node").setValue(sn)
+import nuke
+def nextName(base_name):
+    count = 1
+    while nuke.exists(base_name+str(count)):
+        count += 1
+    return base_name+str(count)
+
+sn = nuke.selectedNode()
+try:
+    vNode = nuke.toNode('VIEWER_INPUT')
+    rname = nextName(vNode.Class())
+    vNode['name'].setValue(rname)
+except:
+    pass
+    
+sn['name'].setValue('VIEWER_INPUT')
