1d4d8ac53a702cb4fb7ec7d8e4d9b1d800d91fad
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-12-30 20:49:16 +0530

    speedyscript full pane

--- SpeedyScript/menu.py
+++ SpeedyScript/menu.py
@@ -1,5 +1,53 @@
-mainMenu = menuMaker()
+import sys
+import nuke
+import os
 
+#*****************************************************************
+#*****************************************************************
+#******************ADD THIS TO YOUR FILE INIT.PY******************
+#SpeedyScript_path = "/Users/yourPath/.nuke/SpeedyScript"
+#nuke.pluginAddPath(SpeedyScript_path)
+#*****************************************************************
+#*****************************************************************
+
+#IMPORT SPEEDY SCRIPT
+import SpeedyScript
+paneMenu = nuke.menu('Pane')
+paneMenu.addCommand('SpeedyScript', SpeedyScript.addSSPanel)
+nukescripts.registerPanel('com.ohufx.SpeedyScript', SpeedyScript.addSSPanel)
+
+
+
+
+#*****************************************************************
+#*****************************************************************
+#CREATE BUTTON IN THE READ NODES when READ NODE is CREATED
+def createReadNodesFunction():
+    import ReadNodes_ImportScript  
+    
+    n = nuke.thisNode()
+
+    
+    metad = 'exr/nuke/script/script' #'exr/nuke/script/script'
+    
+    #CREATE KNOBS
+    tabKnob = nuke.Tab_Knob('Import Script')
+    importKnob = nuke.PyScript_Knob("Import Script into Group","<font color='red'>Import Script into Group","import ReadNodes_ImportScript \nReadNodes_ImportScript.getScriptPath()")
+    openKnob= nuke.PyScript_Knob("Open Script","<font color='deepskyblue'>Open Script","import ReadNodes_ImportScript \nReadNodes_ImportScript.OpenScript()")
+    dividerKnob = nuke.Text_Knob("divider","")
+    
+    n.addKnob(tabKnob)
+    n.addKnob(importKnob)
+    n.addKnob(openKnob)
+    
+#*****************************************************************
+#*****************************************************************  
+#DISABLE/DELETE THE LINE BELOW IF YOU DON'T WANT THE NEW TAB IN EVERY READ NODE
+#nuke.addOnUserCreate(lambda: createReadNodesFunction(), nodeClass="Read")    
+
+
+
+mainMenu = menuMaker()
 nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/import script as Group', "import SpeedyScript ; SpeedyScript.importScriptFunction(nuke.getFilename('Please Select'))", icon='SpeedyScript.png')
 nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Export Group as Grizmo', "import SpeedyScript ; SpeedyScript.exportScriptFunction()", icon='SpeedyScript.png')
 nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Find Imported Group', "import SpeedyScript ; SpeedyScript.findGroupFunction()", icon='SpeedyScript.png')
