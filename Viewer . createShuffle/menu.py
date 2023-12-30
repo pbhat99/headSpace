8a79fb8d18bf91e42da6dc1f2c28d42387de3500
Prasannakumar T Bhat <pbhat99@gmail.com>
2023-12-30 20:08:11 +0530

    shuffle replaced

--- Viewer . createShuffle/menu.py
+++ Viewer . createShuffle/menu.py
@@ -1,2 +1,3 @@
 
 nuke.menu('Viewer').addCommand( 'Create Shuffle', 'import createShuffle ; createShuffle.createShuffle()',icon='Shuffle.png')
+nuke.menu('Nodes').addCommand( "Channel/Shuffle", 'import createShuffle ; createShuffle.createShuffle()',icon='Shuffle.png')
