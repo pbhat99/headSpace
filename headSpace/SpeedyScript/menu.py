88bb2efe999a6354a0fe97b8beac5237d65836df
Prasannakumar T Bhat <pbhat99@gmail.com>
2024-01-03 11:57:38 +0530

    SpeedyScriptLight
    fixed module name

--- headSpace/SpeedyScript/menu.py
+++ headSpace/SpeedyScript/menu.py
@@ -48,6 +48,6 @@ def createReadNodesFunction():
 
 
 mainMenu = menuMaker()
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/import script as Group', "import SpeedyScript ; SpeedyScript.importScriptFunction(nuke.getFilename('Please Select'))", icon='SpeedyScript.png')
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Export Group as Grizmo', "import SpeedyScript ; SpeedyScript.exportScriptFunction()", icon='SpeedyScript.png')
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Find Imported Group', "import SpeedyScript ; SpeedyScript.findGroupFunction()", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/import script as Group', "import SpeedyScriptLight ; SpeedyScriptLight.importScriptFunction(nuke.getFilename('Please Select'))", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Export Group as Grizmo', "import SpeedyScriptLight ; SpeedyScriptLight.exportScriptFunction()", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Find Imported Group', "import SpeedyScriptLight ; SpeedyScriptLight.findGroupFunction()", icon='SpeedyScript.png')
