2e777ac1c94a2bc38f26a797d09a520ece3b430d
Prasannakumar T Bhat <pbhat99@gmail.com>
2024-01-03 12:23:49 +0530

    speedyScript
    search friendly menu

--- headSpace/SpeedyScript/menu.py
+++ headSpace/SpeedyScript/menu.py
@@ -48,6 +48,6 @@ def createReadNodesFunction():
 
 
 mainMenu = menuMaker()
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/import script as Group', "import SpeedyScriptLight ; SpeedyScriptLight.importScriptFunction(nuke.getFilename('Please Select'))", icon='SpeedyScript.png')
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Export Group as Grizmo', "import SpeedyScriptLight ; SpeedyScriptLight.exportScriptFunction()", icon='SpeedyScript.png')
-nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Find Imported Group', "import SpeedyScriptLight ; SpeedyScriptLight.findGroupFunction()", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Speedy import script', "import SpeedyScriptLight ; SpeedyScriptLight.importScriptFunction(nuke.getFilename('Please Select'))", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Speedy Export Group', "import SpeedyScriptLight ; SpeedyScriptLight.exportScriptFunction()", icon='SpeedyScript.png')
+nuke.menu('Nuke').addCommand(mainMenu + 'Speedy Script/Speedy Find Group', "import SpeedyScriptLight ; SpeedyScriptLight.findGroupFunction()", icon='SpeedyScript.png')
