8bffd3a0938c29f0fbbfe0d06a7cbeb31958e67f
Prasannakumar T Bhat <pbhat99@gmail.com>
2025-06-20 18:01:00 +0530

    removed movemenu
    not used for a long time

--- headSpace/W_MoveMenu/menu.py
+++
@@ -1,6 +0,0 @@
-import W_moveMenu
-
-#mainMenu = menuMaker()
-mainMenu = os.path.dirname(__file__).split('/')[-2]
-
-W_moveMenu.moveMenus(['Edit/W_hotbox', 'Edit/Node/Open Floating Knob Scripter', 'Edit/Stamps' ], mainMenu, sourceRoot = 'Nuke', destinationRoot = 'Nuke')
