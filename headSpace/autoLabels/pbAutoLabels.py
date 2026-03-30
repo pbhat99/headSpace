119f6d2a691d14c8823e9e6968382031a0947314
Prasannakumar T Bhat <pbhat99@gmail.com>
2026-03-30 18:26:26 +0530

    autolabel cleanups
    and few additions

--- headSpace/autoLabels/pbAutoLabels.py
+++ headSpace/autoLabels/pbAutoLabels.py
@@ -1,9 +1,12 @@
 import nuke
 import os
+import math
 
 def trimTo2Dec(value):
     def _one(x):
-        s = "{:.2f}".format(x)
+        # Round up to 2 decimal places
+        x_rounded = math.ceil(x * 100.0) / 100.0
+        s = f"{x_rounded:.2f}"
         return s.rstrip('0').rstrip('.') if '.' in s else s
     if isinstance(value, (list, tuple)):
         return ", ".join(_one(x) for x in value)
@@ -24,78 +27,96 @@ def pbAutoLabel():
 
     if n.Class() == "Defocus":
         addIndicators()
-        aLabel.append(n.name() + ' [' + str(round(n['defocus'].value(), 2)).strip('.0')+ ']')
-        aLabel if n["channels"].value() == "rgba" else aLabel.append('(' + str(n["channels"].value() + ')'))
+        aLabel.append(f"{n.name()} [{trimTo2Dec(n['defocus'].value())}]")
+        if n["channels"].value() != "rgba":
+            aLabel.append(f'({n["channels"].value()})')
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
     if n.Class() in ("Blur", "Erode", "Dilate", "FilterErode", "EdgeBlur", "Median", "ZDefocus2", "Sharpen", "Soften"):
         addIndicators()
-        aLabel.append(n.name() + ' [' + trimTo2Dec(n['size'].value()) + ']')
-        aLabel if n["channels"].value() == "rgba" else aLabel.append('(' + str(n["channels"].value() + ')'))
+        aLabel.append(f"{n.name()} [{trimTo2Dec(n['size'].value())}]")
+        if n["channels"].value() != "rgba":
+            aLabel.append(f'({n["channels"].value()})')
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
-    if n.Class() in ("Transform"):
+    if n.Class() in ("Transform", "CornerPin2D"):
         addIndicators()
         aLabel.append(n.name())
-        aLabel if n["invert_matrix"].value() == False else aLabel.append('[inverted]')
-        aLabel if n["motionblur"].value() == False else aLabel.append('[mb : ' + str(n['motionblur'].value()).rstrip('.0') + ' ~ ' +  str(n['shutter'].value()).rstrip('.0') + ']')
-        aLabel.append(n["label"].evaluate() or "")
-        return "\n".join(aLabel).strip()
-
-    if n.Class() in ("CornerPin2D"):
-        addIndicators()
-        aLabel.append(n.name())
-        aLabel if n["invert"].value() == False else aLabel.append('[inverted]')
-        aLabel if n["motionblur"].value() == False else aLabel.append('[mb : ' + str(n['motionblur'].value()).rstrip('.0') + ' ~ ' +  str(n['shutter'].value()).rstrip('.0') + ']')
+        # Check for inverted state (different knob names per node)
+        inv = n.knob("invert_matrix") or n.knob("invert")
+        if inv and inv.value():
+            aLabel.append('[inverted]')
+        if n["motionblur"].value():
+            aLabel.append(f"[mb : {str(n['motionblur'].value()).rstrip('.0')} ~ {str(n['shutter'].value()).rstrip('.0')}]")
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
     if n.Class() in ("Tracker4"):
         addIndicators()
-        aLabel.append(n.name() + ' [' + trimTo2Dec(n['reference_frame'].value()) + ']' )
-        aLabel if n["transform"].value() == 'none' else aLabel.append(n["transform"].value())
-        aLabel if n["motionblur"].value() == False else aLabel.append('[mb : ' + str(n['motionblur'].value()).rstrip('.0') + ' ~ ' +  str(n['shutter'].value()).rstrip('.0') + ']')
+        aLabel.append(f"{n.name()} [{trimTo2Dec(n['reference_frame'].value())}]")
+        if n["transform"].value() != 'none':
+            aLabel.append(n["transform"].value())
+        if n["motionblur"].value():
+            aLabel.append(f"[mb : {str(n['motionblur'].value()).rstrip('.0')} ~ {str(n['shutter'].value()).rstrip('.0')}]")
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
 
-    # if n.Class() == "Mirror2":
-    #     addIndicators()
-    #     if n["flip"].value() == True:
-    #         icnFlip = '[&#8644;]'
-    #     elif n["flop"].value() == True:
-    #         icnFlop = '[&#8645;]'
-    #     else:
-    #         icnFlip = ''
-    #         icnFlop = ''
-    #     aLabel.append(n.name())
-    #     aLabel.append(icnFlip + icnFlop)
-    #     aLabel.append(n["label"].evaluate() or "")
-    #     return "\n".join(aLabel).strip()
-
-    if n.Class() == "TimeClip":
+    if n.Class() in ("Mirror", "Mirror2"):
         addIndicators()
         aLabel.append(n.name())
-        aLabel.append('[' + str(n['first'].value()) + '-' + str(n['last'].value())+ ']')
+        m_label = []
+        if n["flip"].value():
+            m_label.append("vertical")
+        if n["flop"].value():
+            m_label.append("horizontal")
+        if m_label:
+            aLabel.append(f"[{' & '.join(m_label)}]")
+        aLabel.append(n["label"].evaluate() or "")
+        return "\n".join(aLabel).strip()
+
+    if n.Class() in ("Read", "DeepRead", "TimeClip"):
+        addIndicators()
+        aLabel.append(n.name())
+        
+        file_knob = n.knob("file")
+        range_str = f"[{int(n['first'].value())}-{int(n['last'].value())}]"
+        
+        if file_knob and file_knob.value():
+            filename = os.path.basename(file_knob.value())
+            aLabel.append(filename)
+            
+        if n.knob("raw") and n["raw"].value():
+            aLabel.append("[RAW]")
+        elif n.knob("colorspace"):
+            aLabel.append(f"[{n['colorspace'].value()}]")
+        
+        aLabel.append(range_str)
+            
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
     if n.Class() == "ScanlineRender":
         addIndicators()
-        aLabel.append(n.name() + ' [' + str(int(n['samples'].value()))+ ']')
-        aLabel if n["projection_mode"].value() == "render camera" else aLabel.append('[' + str(n["projection_mode"].value()) + ']')
+        aLabel.append(f"{n.name()} [{int(n['samples'].value())}]")
+        if n["projection_mode"].value() != "render camera":
+            aLabel.append(f'[{n["projection_mode"].value()}]')
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
 
     if n.Class() in ("Remove"):
         addIndicators()
-        aLabel.append(n.name() + ' [' + str(n['operation'].value())+ ']')
-        aLabel if n["channels"].value() == "none" else aLabel.append('(' + str(n["channels"].value() + ')'))
-        aLabel if n["channels2"].value() == "none" else aLabel.append('(' + str(n["channels2"].value() + ')'))
-        aLabel if n["channels3"].value() == "none" else aLabel.append('(' + str(n["channels3"].value() + ')'))
-        aLabel if n["channels4"].value() == "none" else aLabel.append('(' + str(n["channels4"].value() + ')'))
+        aLabel.append(f"{n.name()} [{n['operation'].value()}]")
+        if n["channels"].value() != "none":
+            aLabel.append(f'({n["channels"].value()})')
+        if n["channels2"].value() != "none":
+            aLabel.append(f'({n["channels2"].value()})')
+        if n["channels3"].value() != "none":
+            aLabel.append(f'({n["channels3"].value()})')
+        if n["channels4"].value() != "none":
+            aLabel.append(f'({n["channels4"].value()})')
         aLabel.append(n["label"].evaluate() or "")
         return "\n".join(aLabel).strip()
        
