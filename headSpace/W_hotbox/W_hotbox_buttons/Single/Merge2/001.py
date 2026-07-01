f65b0e3ec0e37a621eb459af3d58ff216e6a7f6a
Prasannakumar T Bhat <pbhat99@gmail.com>
2026-07-01 14:07:41 +0530

    hotbox
    chromamerge and screen/plus togle

--- headSpace/W_hotbox/W_hotbox_buttons/Single/Merge2/001.py
+++ headSpace/W_hotbox/W_hotbox_buttons/Single/Merge2/001.py
@@ -2,9 +2,14 @@
 #
 # AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
 #
-# NAME: Screen
+# NAME: Screen/Plus
 #
 #----------------------------------------------------------------------------------------------------------
 
 for i in nuke.selectedNodes():
-    i.knob('operation').setValue('screen')
+    if i.knob('operation').value() == 'screen':
+        i.knob('operation').setValue('plus')
+        i.knob('output').setValue('rgb')
+    else:
+        i.knob('operation').setValue('screen')
+        i.knob('output').setValue('rgba')
